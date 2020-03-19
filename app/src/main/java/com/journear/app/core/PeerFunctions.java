2a2fab14f851966f218de5e0000dd2e5bc557103
Nikhil Girraj <khandeln@tcd.ie>
2020-03-19 21:25:19 +0530

    Added a shell function for getNearbyDevices().

--- app/src/main/java/com/journear/app/core/PeerFunctions.java
+++ app/src/main/java/com/journear/app/core/PeerFunctions.java
@@ -1,4 +1,13 @@
 package com.journear.app.core;
 
+import com.journear.app.core.entities.NearbyDevices;
+
+import java.util.LinkedList;
+
 public class PeerFunctions {
+
+    public static LinkedList<NearbyDevices> getNearbyDevices() {
+        throw new UnsupportedOperationException("implementation pending");
+    }
+
 }
